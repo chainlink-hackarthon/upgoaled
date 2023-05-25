0640751e2d1a9071264d9a7b42e31dcab3218186
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-05-25 09:23:08 -0600

    bassic structure

--- upgoaled.sol
+++ upgoaled.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^8.0.8;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract Upgoaled {
+
+    // User struct to store user information
+    struct User {
+
+    }
+    // Goal struct to store goal information
+    struct Goal {
+
+    }
+    // GoalPool struct to store goal pool information
+    struct GoalPool {
+
+    }
+    // Function to create a new goal pool (Only Owner)
+    function createGoalPool() public onlyOwner{
+
+    }
    
 }
